1e385516c4c04cf6d2f02ee3cc321eb7b878d7c8
Snehachill <st957553@gmail.com>
2026-08-12 22:15:21 +0530

    Time: 2 ms (76.23%), Space: 42.9 MB (92.15%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        int[] freq = new int[128];
+        
+        for (int i = 0; i < s.length(); i++) {
+            freq[s.charAt(i)]++;
+        }
+        
+        int length = 0;
+        boolean hasOdd = false;
+        
+        for (int count : freq) {
+            length += (count / 2) * 2;
+            
+            if (count % 2 == 1) {
+                hasOdd = true;
+            }
+        }
+        
+        if (hasOdd) {
+            length += 1;
+        }
+        
+        return length;
+    }
+}
