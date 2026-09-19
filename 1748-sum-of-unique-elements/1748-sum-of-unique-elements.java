d241200ed8d4e3593e96e90fcc1859c152d7334a
Snehachill <st957553@gmail.com>
2026-09-19 20:49:23 +0530

    Time: 0 ms (100%), Space: 43.4 MB (7.23%) - LeetHub

---
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int sumOfUnique(int[] nums) {
+        int freq[]=new int[101];
+        for(int i=0;i<nums.length;i++){
+            freq[nums[i]]++;
+        }
+
+        int sum=0;
+        for(int i=0;i<nums.length;i++){
+            if(freq[nums[i]]==1){
+                sum=sum+nums[i];
+            }
+        }
+        return sum;
+    }
+}
